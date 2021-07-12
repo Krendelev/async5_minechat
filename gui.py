a0a9796acdfa9caabf4e6732c6b352ac036fa652
GK-dvmn <krendelev@gmail.com>
2021-07-12 03:59:50 +0300

    implement TODO

--- gui.py
+++ gui.py
@@ -55,11 +55,10 @@ async def update_conversation_history(panel, messages_queue):
         if panel.index("end-1c") != "1.0":
             panel.insert("end", "\n")
         panel.insert("end", msg)
-        # TODO сделать промотку умной, чтобы не мешала просматривать историю сообщений
-        # ScrolledText.frame
-        # ScrolledText.vbar
-        panel.yview(tk.END)
         panel["state"] = "disabled"
+        scrollbar_pos = panel.vbar.get()
+        if scrollbar_pos[1] == 1.0:
+            panel.yview(tk.END)
 
 
 async def update_status_panel(status_labels, status_updates_queue):
